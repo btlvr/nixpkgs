5d49b60f3db01b8c325d61926c4fa62fcbda77a8
Your Name <you@example.com>
2020-02-04 09:19:35 -0800

    created exiftool package

---
+++ pkgs/tools/system/exifprobe/default.nix
@@ -0,0 +1,14 @@
+{ stdenv, fetchFromGitHub }:
+stdenv.mkDerivation rec {
+  pname = "exifprobe";
+  version = "2.1.0";
+
+  installFlags = [ "DESTDIR=$(out)" ];
+
+  src = fetchFromGitHub {
+    owner = "hfiguiere";
+    repo = "exifprobe";
+    rev = "ce1ea2bc3dbbe8092b26f41cd89831cafe633d69";
+    sha256 = "1c1fhc0v1m452lgnfcijnvrc0by06qfbhn3zkliqi60kv8l2isbp";
+  };
+}
